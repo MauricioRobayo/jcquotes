1dc26ee768dec85b62887296ae8a0325403d5307
Mauricio Robayo <rfmajo@gmail.com>
2022-05-18 15:56:18 -0500

    Dark theme for preferred color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,11 @@ module.exports = {
   ],
   theme: {
     colors: {
+      "gold-0": "#c7c1b4",
       "gold-1": "#ece8df",
-      "gold-2": "#322401",
+      "gold-2": "#5e5b55",
+      "gold-3": "#504e49",
+      "gold-4": "#38352e",
     },
     fontFamily: {
       sans: ["'Nunito Sans', sans-serif"],
