281678a079d9a0b0ae63fea3ce5892a408184de9
Mauricio Robayo <rfmajo@gmail.com>
2022-05-17 23:19:14 -0500

    Update seed script

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -1,27 +1,53 @@
+import axios from "axios";
 import dotenv from "dotenv";
-import { getQuotes } from "jcscraper";
-import { Quote } from "jcscraper/dist/getQuotes";
+import { getQuotes, QuoteType } from "jcscraper";
+import { MongoClient } from "mongodb";
 import path from "path";
 
 dotenv.config({
   path: path.join(process.cwd(), ".env.local"),
 });
 
+const mongoClient = new MongoClient(process.env.MONGODB_URI ?? "");
 async function seed(): Promise<void> {
+  await mongoClient.connect();
+  await mongoClient
+    .db("jc-quotes")
+    .collection("quotes")
+    .createIndex({ clickToTweetId: 1 }, { unique: true });
+  mongoClient.close();
+
   const quotes = await getQuotes();
 
-  try {
-    const result = await collection.insertMany(quotes);
-    console.log(`Done! Inserted ${result.insertedCount} quotes.`);
-    client.close();
-  } catch (err) {
-    client.close();
-    if (err instanceof MongoBulkWriteError) {
-      console.log(err.message);
-      return;
+  const chunk: QuoteType[] = [];
+  const chunkSize = 20;
+  for (const quote of quotes) {
+    chunk.push(quote);
+    if (chunk.length >= chunkSize) {
+      console.log(`Posting ${chunk.length} quotes...`);
+      await Promise.all(chunk.map(postQuote));
+      chunk.length = 0;
     }
+  }
 
-    throw err;
+  if (chunk.length > 0) {
+    console.log(`Posting ${chunk.length} quotes...`);
+    await Promise.all(chunk.map(postQuote));
+  }
+}
+
+async function postQuote(quote: QuoteType) {
+  try {
+    const { data } = await axios.post<{ id: string }>(
+      `${process.env.API_URL}?key=${process.env.API_KEY}`,
+      quote
+    );
+    return data;
+  } catch (err) {
+    if (axios.isAxiosError(err) && err.response?.status === 409) {
+      console.log(`Quote id ${quote.clickToTweetId} already in database!`);
+      return { error: err.code };
+    }
   }
 }
 
