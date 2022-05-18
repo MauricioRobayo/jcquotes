82760af624ea23cbce2b1e44ef0321e4a2fa7f73
Mauricio Robayo <rfmajo@gmail.com>
2022-05-18 09:04:22 -0500

    Fix double initial loading

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,18 @@
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
-import Quote from "../components/Quote";
 import QuoteLoader from "../components/QuoteLoader";
 import { useRandomQuote } from "../hooks/useRandomQuote";
 
 const Home: NextPage = () => {
   const router = useRouter();
-  const { data, isIdle, isLoading, isError } = useRandomQuote();
+  const { data, isLoading, isError } = useRandomQuote();
 
   useEffect(() => {
     if (data) {
       router.replace(`/${data.clickToTweetId}`);
     }
   }, [data, router]);
-
-  if (isIdle) {
-    return null;
-  }
 
   if (isLoading) {
     return <QuoteLoader />;
@@ -27,7 +22,7 @@ const Home: NextPage = () => {
     return <div>Something unexpected happened!</div>;
   }
 
-  return <Quote id={data.clickToTweetId} />;
+  return null;
 };
 
 export default Home;
