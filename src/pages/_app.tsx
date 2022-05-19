a8dab996b93a320f875ad9ff82008dfac40b633a
Mauricio Robayo <rfmajo@gmail.com>
2022-05-19 14:52:36 -0500

    Add google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,17 +3,22 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
+import { GoogleAnalytics, usePagesViews } from "nextjs-google-analytics";
 
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
+  usePagesViews();
   return (
-    <QueryClientProvider client={queryClient}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-      <ReactQueryDevtools />
-    </QueryClientProvider>
+    <>
+      <GoogleAnalytics />
+      <QueryClientProvider client={queryClient}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </>
   );
 }
 
