d117e13444995d798e59200b29ce7488520a1bba
Mauricio Robayo <rfmajo@gmail.com>
2022-05-18 20:20:41 -0500

    Improve QuoteLoader styles

--- src/components/QuoteLoader.tsx
+++ src/components/QuoteLoader.tsx
@@ -6,13 +6,13 @@ const QuoteLoader = () => {
     <QuoteContainer>
       <div className={styles.quote}>
         <div className="animate-pulse flex space-x-4">
-          <div className="flex-1 space-y-6 py-1">
-            <div className="h-2 bg-gold-2/20 rounded"></div>
+          <div className="flex-1 space-y-4 py-1">
+            <div className="h-3 bg-gold-2/20 dark:bg-gold-1/20 rounded"></div>
             <div className="grid grid-cols-3 gap-4">
-              <div className="h-2 bg-gold-2/20 rounded col-span-2"></div>
-              <div className="h-2 bg-gold-2/20 rounded col-span-1"></div>
+              <div className="h-3 bg-gold-2/20 dark:bg-gold-1/20 rounded col-span-2"></div>
+              <div className="h-3 bg-gold-2/20 dark:bg-gold-1/20 rounded col-span-1"></div>
             </div>
-            <div className="h-2 bg-gold-2/20 rounded"></div>
+            <div className="h-3 bg-gold-2/20 dark:bg-gold-1/20 rounded"></div>
           </div>
         </div>
       </div>
