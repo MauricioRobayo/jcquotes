d364152e0f5e14fda5fc00d71093498df2f5fb5c
Mauricio Robayo <rfmajo@gmail.com>
2024-03-10 16:53:04 -0500

    Refactor Blockquote component imports and class names

--- src/components/Blockquote.tsx
+++ src/components/Blockquote.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode } from "react";
-import cn from "clsx";
 import { Crimson_Pro } from "next/font/google";
+import { twMerge } from "tailwind-merge";
 
 const crimsonPro = Crimson_Pro({
   subsets: ["latin"],
@@ -14,7 +14,7 @@ type BlockquoteWrapperProps = {
 const Blockquote = ({ children, className = "" }: BlockquoteWrapperProps) => {
   return (
     <blockquote
-      className={cn(
+      className={twMerge(
         crimsonPro.variable,
         "p-4 bg-gold-1 text-gold-2 dark:bg-gold-3 dark:text-gold-0 rounded-md font-serif text-xl shadow-md space-y-2 font-light",
         className
